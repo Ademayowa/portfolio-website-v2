5f60ebf9aee744e4e3b3d6caa78d3502b28881b8
Ademayowa <ademayowaadeniyi@gmail.com>
2022-02-05 12:37:36 +0100

    completed projects section

--- components/Title.js
+++ components/Title.js
@@ -1,7 +1,7 @@
 export default function Title({ title }) {
   return (
     <div>
-      <h2 className='text-center fw-bold mt-lg-5'>{title}</h2>
+      <h2 className='text-center fw-bold mt-5'>{title}</h2>
     </div>
   );
 }
