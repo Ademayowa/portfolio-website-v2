553461b16244033bb221a55e6b42010f47126e05
Ademayowa <ademayowaadeniyi@gmail.com>
2023-08-25 20:40:05 +0100

    updates on hero page

--- components/Hero.js
+++ components/Hero.js
@@ -9,7 +9,7 @@ export default function Hero() {
       <Container>
         <Row>
           <Col lg={7}>
-            <h1>Hello 👋</h1>
+            <h1>Hello!</h1>
             <h2>My name is Mayowa Adeniyi</h2>
             <Col md={10} lg={10} className='ps-0 mt-4 mb-2 fs-4 pb-2'>
               <p>
