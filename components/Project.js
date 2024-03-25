334f8f0c2879dfb520167ca2cfc4ca534798ac6b
Mayowa <30834433+Ademayowa@users.noreply.github.com>
2024-03-25 14:37:21 -0700

    chore: updated project title

--- components/Project.js
+++ components/Project.js
@@ -8,7 +8,7 @@ export default function Project({ project }) {
   return (
     <section className={styles.project} id='projects'>
       <Container>
-        <Title title='Some of my freelance projects' />
+        <Title title='Some freelance projects' />
         <div className='center-line mb-5' />
         <Row className='gx-2'>
           {project.map((project) => (
