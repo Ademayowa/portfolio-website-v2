c61aafe4c72ba978a42ff11456e94af63be6dedf
Mayowa <30834433+Ademayowa@users.noreply.github.com>
2025-01-01 10:42:56 -0800

    chore: update footer

--- components/Footer.js
+++ components/Footer.js
@@ -22,7 +22,7 @@ export default function Footer() {
         </div>
 
         <Link href='#top'>
-          <a className='text-white'>&copy; 2025. Built by Mayowa Adeniyi</a>
+          <a className='text-white'>&copy; 2025. All rights reserved. Built by Mayowa Adeniyi</a>
         </Link>
       </Container>
     </footer>
