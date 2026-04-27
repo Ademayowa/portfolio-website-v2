70919701e2d58020af202ce79766513a8f6b0033
Mayowa <30834433+Ademayowa@users.noreply.github.com>
2026-04-27 11:24:13 +0100

    refactor: refactor ProjectList component to simplify display
    Removed technologies display and source code link.

--- components/ProjectList.js
+++ components/ProjectList.js
@@ -1,10 +1,9 @@
-import { Col, Card, Text, Badge } from 'react-bootstrap';
-import Link from 'next/link';
-import { FaCodeBranch, FaEye } from 'react-icons/fa';
+import { Col, Card } from 'react-bootstrap';
+import { FaEye } from 'react-icons/fa';
 import styles from '@/styles/ProjectList.module.css';
 
 export default function ProjectList({
-  project: { info, technologies, title, url, repo },
+  project: { info, title, url },
 }) {
   return (
     <Col md={6} lg={4}>
@@ -12,22 +11,11 @@ export default function ProjectList({
         <Card.Body>
           <Card.Title className={styles.ptitle}>{title}</Card.Title>
           <Card.Text className={styles.pinfo}>{info}</Card.Text>
-          <h5 className='mt-4'>Technologies</h5>
-          {technologies.map((tech, index) => (
-            <Badge key={index} bg='secondary' className={styles.tech}>
-              {tech}
-            </Badge>
-          ))}
 
           <div className={styles.picons}>
             <a href={url} target='_blank' rel='noopener noreferrer'>
-              <FaEye /> View Project
+              <FaEye /> View Blog
             </a>
-            {repo && (
-              <a href={repo} target='_blank' rel='noopener noreferrer'>
-                <FaCodeBranch className='ms-3' /> Source Code
-              </a>
-            )}
           </div>
         </Card.Body>
       </Card>
